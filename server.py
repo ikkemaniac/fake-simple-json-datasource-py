16a56fdf25d8eb367d646f7a5086563c55ea5558
ikkemaniac <ikkemaniac@localhost>
2016-12-21 01:42:04 +0100

    remove unnecessary code

--- server.py
+++ server.py
@@ -45,43 +45,20 @@ class myHandler(BaseHTTPRequestHandler):
 	
 	#~ GET requests
 	def do_GET(self):
-		if self.path=="/":
-			self.path="/index.html"
-
 		try:
-			#Check the file extension required and
-			#set the right mime type
+			#~ When the webroot gets hit return a 200, OK
+			if self.path=="/":
+				self.send_response(200)
+				self.send_header('Content-type','text/html')
+				self.end_headers()
+				self.wfile.write("Hello World !")
 
-			sendReply = False
-			if self.path.endswith(".html"):
-				mimetype='text/html'
-				sendReply = True
-			if self.path.endswith(".jpg"):
-				mimetype='image/jpg'
-				sendReply = True
-			if self.path.endswith(".gif"):
-				mimetype='image/gif'
-				sendReply = True
-			if self.path.endswith(".js"):
-				mimetype='application/javascript'
-				sendReply = True
-			if self.path.endswith(".css"):
-				mimetype='text/css'
-				sendReply = True
-
-			if (self.path == "/search"):
+			#~ When 'search' gets hit return list of available targets
+			elif (self.path == "/search"):
 				data = []
 				for row in data_series:
 					data.append (row["target"])
 				self.request.sendall(json.dumps(data))
-			elif sendReply == True:
-				#Open the static file requested and send it
-				f = open(curdir + sep + self.path) 
-				self.send_response(200)
-				self.send_header('Content-type',mimetype)
-				self.end_headers()
-				self.wfile.write(f.read())
-				f.close()
 			return
 
 
